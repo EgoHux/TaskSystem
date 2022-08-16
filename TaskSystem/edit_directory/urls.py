1498e55a3f70d113ff8c1f1227051309d88b0636
egor <EgoHux>
2022-08-17 00:09:18 +0700

    Доделана логика админа

--- TaskSystem/edit_directory/urls.py
+++ TaskSystem/edit_directory/urls.py
@@ -7,6 +7,10 @@ app_name = "edit_directory"
 urlpatterns = [
 
     path("", edit_directory.view, name='directory_view'),
+    path("create_status", edit_directory.create_status, name='create_status'),
     path("edit_status/<int:status_id>", edit_directory.edit_status, name='edit_status'),
-    path('edit_tasktype/<int:tasktype_id>', edit_directory.edit_tasktype, name='edit_tasktype')
+    path('delete_status/<int:pk>', edit_directory.delete_status, name="delete_status"),
+    path("create_tasktype", edit_directory.create_tasktype, name='create_tasktype'),
+    path('edit_tasktype/<int:tasktype_id>', edit_directory.edit_tasktype, name='edit_tasktype'),
+    path('delete_tasktype/<int:pk>', edit_directory.delete_tasktype, name="delete_tasktype"),
 ]
--- TaskSystem/edit_directory/views.py
+++ TaskSystem/edit_directory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from edit_directory.models import Status, TaskType
 from edit_directory.forms import StatusForm, TaskTypeForm
@@ -16,6 +16,19 @@ def view(request):
 
     })
 
+def create_status(request):
+    form = StatusForm()
+    if request.method == 'POST':
+        form = StatusForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('edit_directory:directory_view'))
+    return render(request, 'edit_directory/create_status.html', context={
+        'title':"Создание статуса",
+        'form':form
+    })
+
+
 def edit_status(request, status_id):
     status=Status.objects.get(id=status_id)
     form = StatusForm(instance=status)
@@ -27,6 +40,19 @@ def edit_status(request, status_id):
 
     return render(request, 'edit_directory/edit_status.html', context={
         'form': form
+    })
+
+
+def create_tasktype(request):
+    form = TaskTypeForm()
+    if request.method == 'POST':
+        form = TaskTypeForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('edit_directory:directory_view'))
+    return render(request, 'edit_directory/create_tasktype.html', context={
+        'title':"Создание типа заявки",
+        'form':form
     })
 
 def edit_tasktype(request, tasktype_id):
@@ -42,3 +68,13 @@ def edit_tasktype(request, tasktype_id):
         'form': form
     })
 
+
+def delete_status(request, pk):
+    status = get_object_or_404(Status, pk=pk)
+    status.delete()
+    return HttpResponseRedirect(reverse("edit_directory:directory_view"))
+
+def delete_tasktype(request, pk):
+    tasktype = get_object_or_404(TaskType, pk=pk)
+    tasktype.delete()
+    return HttpResponseRedirect(reverse("edit_directory:directory_view"))
